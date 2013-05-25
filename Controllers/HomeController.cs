2294c6eb13763217939e111d86b15dfb0186f9a7
Scott Weinstein <scottwww@squidnet.com>
2013-05-24 21:35:50 -0400

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Office.Interop.Word;
+
+namespace MathEquationsViaWord.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string eq)
+        {
+            if (string.IsNullOrWhiteSpace(eq)) 
+            {
+                return Redirect("/E=MC^2");
+            }
+
+            using (var fic = new FormulaImageConverter(new Application()))
+            {
+                var bytes = fic.ConvertFormulaToImage(eq);
+                return File(bytes, "image/png"); ;
+            }
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Routing;
+
+namespace MathEquationsViaWord
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
